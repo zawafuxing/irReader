10957ab7cdefbb85ed7c151999793274d14039e1
Fuxing Xue <fuxingxue@FuxingdeMac-mini.local>
2017-02-18 08:08:02 +0800

    Initial Commit

---
+++ irReader/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  irReader
+//
+//  Created by Fuxing Xue on 2017/2/18.
+//  Copyright © 2017年 Fuxing Xue. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
